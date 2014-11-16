dc6be3e4da9b100ad69e6bb42cbd0abe12564e2e
nlare77 <nicklare@gmail.com>
2014-11-16 14:55:55 -0500

    clickonItemList

---
+++ app/src/main/java/com/n/carapp/MainActivity.java
@@ -0,0 +1,95 @@
+package com.n.carapp;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+    ArrayList<String> carBrandNameList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+        ListView carBrandList = (ListView)findViewById(R.id.listViewBrands);
+
+        carBrandNameList = new ArrayList<String>();
+        getBrandNames();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, carBrandNameList);
+        carBrandList.setAdapter(arrayAdapter);
+
+        // START HERE JASON, if you comment this out, ListView works fine
+        carBrandList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                Intent intent = new Intent (this, DetailsActivity.class);
+                startActivity(intent);
+            }
+        }
+
+
+
+
+    }
+    void getBrandNames()
+    {
+        carBrandNameList.add("Ferrari");
+        carBrandNameList.add("Tesla");
+        carBrandNameList.add("Lamborghini");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
