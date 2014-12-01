063d887a5fff7418089ac19aab8153976fb61e3b
nlare77 <nicklare@gmail.com>
2014-11-30 21:02:44 -0500

    changed temp image

--- app/src/main/java/com/n/carapp/ImageLoader.java
+++ app/src/main/java/com/n/carapp/ImageLoader.java
@@ -39,7 +39,7 @@ public class ImageLoader {
         executorService = Executors.newFixedThreadPool(5);
     }
 
-    final int stub_id = R.drawable.diablo;
+    final int stub_id = R.drawable.teslamodels;
 
     public void DisplayImage(String url, ImageView imageView) {
         imageViews.put(imageView, url);
